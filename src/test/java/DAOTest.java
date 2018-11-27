e1c673cd6dcf9278c8b3487edb4643edfa1941f1
NathanVaty <nvaty@etud.univ-jfc.fr>
2018-11-27 15:15:22 +0100

    changement emplacement fichier test

--- src/test/java/DAOTest.java
+++ src/test/java/DAOTest.java
@@ -4,6 +4,12 @@
  * and open the template in the editor.
  */
 
+import java.util.Date;
+import java.util.HashMap;
+import javax.sql.DataSource;
+import modele.DAOadmin;
+import modele.DAOclient;
+import modele.DataSourceFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,29 +22,75 @@ import static org.junit.Assert.*;
  * @author pierr
  */
 public class DAOTest {
+    private DAOadmin myDaoAdmin; // Dao admin
+    private DAOclient myDaoClient; // Dao client
     
-    public DAOTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    private DataSource myDataSource; // La source de données à utiliser 
     
     @Before
     public void setUp() {
+        myDataSource = DataSourceFactory.getDataSource();
+        myDaoAdmin = new DAOadmin(myDataSource);
+        myDaoClient = new DAOclient(myDataSource);
     }
     
-    @After
-    public void tearDown() {
+    /* ======================== Test Admin ================================== */
+    /**
+     * Méthode de test pour obtenir le CA selon une categorie de produit
+     */
+    @Test
+    public void testCACatego() {
+        // TODO implementer test + changer val retour
+        String productCode = "TT";
+        Date dateDeb = new Date("2018/11/30");
+        Date dateFin = new Date("2018/11/30");
+        HashMap<String,Double> result = myDaoAdmin.CAofCategorie(productCode,dateDeb,dateFin); //bouchon
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    /**
+     * Méthode de test pour obtenir le CA selon une zone géographique
+     */
+    @Test
+    public void testCAZoneGeo() {
+        // TODO implementer test + changer val retour
+        String zipCode = "12345";
+         Date dateDeb = new Date("2018/11/30");
+        Date dateFin = new Date("2018/11/30");
+         HashMap<String,Double> result = myDaoAdmin.CAofZoneGeo(zipCode, dateDeb, dateFin); //bouchon
+    }
+    
+    /**
+     * Méthode de test pour obtenir le CA selon un client
+     */
+    @Test
+    public void testCAClient() {
+        //TODO Implementer + changer val retour
+        int codeClient = 5;
+        Date dateDeb = new Date("2018/11/30");
+        Date dateFin = new Date("2018/11/30");
+         HashMap<String,Double> result = myDaoAdmin.CAfromClient(codeClient, dateDeb, dateFin); //bouchon
+    }
+    
+    @Test
+    public void testInsertProduct() {
+        // TODO Implementer + changer val retour
+        int result = myDaoAdmin.insertProduct(); //bouchon
+    }
+    
+    public void testUpdateProduct(){
+        //TODO implementer + changer val retour
+        int result = myDaoAdmin.updateProduct(); //bouchon
+    }
+    
+    public void testDeleteProduct(){
+        //TODO implementer + changer val retour
+        int result = myDaoAdmin.deleteProduct(); // bouchon
+    }
+    
+    
+    /* ====================================================================== */
+    /* ======================== Test Client ================================= */
+    
 }
+
+
