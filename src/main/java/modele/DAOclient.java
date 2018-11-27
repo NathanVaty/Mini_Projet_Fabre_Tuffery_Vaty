2cc6f677c1e24480bc21583dcd6566d5841473b8
Morgane Tuffery <morgane.tuffery@etud.univ-jfc.fr>
2018-11-27 15:07:02 +0100

    27/11
    Modification du fihcier DAOclient, création des entetes de fonctions.

--- src/main/java/modele/DAOclient.java
+++ src/main/java/modele/DAOclient.java
@@ -5,11 +5,74 @@
  */
 package modele;
 
+import javax.sql.DataSource;
+
 /**
- *
- * @author vanat
+ * @author Morgane Tuffery
  */
 public class DAOclient {
     
+    protected final DataSource myDataSource;
+    
+    public DAOclient(DataSource dataSource){
+        this.myDataSource = dataSource;
+    }
+    
+    /**
+     * Fonction permettant aux clients de modifier ses données personnelles.
+     * @param customerId
+     * @param zip 
+     * @param name
+     * @param addressLine1
+     * @param addressLine2
+     * @param city
+     * @param state
+     * @param phone
+     * @param fax
+     * @param email 
+     */
+    public void editPersonnalData(int customerId , String zip, String name, String addressLine1 ,
+                                  String addressLine2,String city, String state,
+                                  String phone, String fax, String email){
+        
+    }
+    
+    /**
+     * Fonction permettant au client d'ajouter un bon de commande
+     * @param customerId
+     * @param quantite
+     * @param shippingCost
+     * @param salesDate
+     * @param shippingDate
+     * @param freightCompany 
+     */
+    public void addPurchaseOrder(int customerId ,int quantite, float shippingCost, String salesDate,
+                                 String shippingDate, String freightCompany){
+        
+    }
+    
+    /**
+     * Fonction permettant aux clients de changer leurs bons de commande
+     * @param customerId
+     * @param quantite
+     * @param shippingCost
+     * @param salesDate
+     * @param shippingDate
+     * @param freightCompany 
+     */
+    public void editPurchaseOrder(int customerId ,int quantite, float shippingCost, String salesDate,
+                                 String shippingDate, String freightCompany){
+        
+    }
+    
+    /**
+     * Fonction permettant aux clients de supprimer leurs bons de commande
+     * @param orderId 
+     */
+    public void deletePurchaseOrder(int orderId){
+        
+    }
+    
+            
 }
 
