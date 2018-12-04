d0554dbd187541cda7e511cce8aa83e38145663e
Pierrick Fabre <pierrick.fabre@etud.univ-jfc.fr>
2018-12-04 11:26:54 +0100

    Modification Test + Test CAcat finis 28/11

--- src/test/java/DAOTest.java
+++ src/test/java/DAOTest.java
@@ -16,6 +16,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
@@ -40,57 +41,92 @@ public class DAOTest {
      */
     @Test
     public void testCACatego() {
-        // TODO implementer test + changer val retour
-        String productCode = "TT";
-        Date dateDeb = new Date("2018/11/30");
-        Date dateFin = new Date("2018/11/30");
-        HashMap<String,Double> result = myDaoAdmin.CAofCategorie(productCode,dateDeb,dateFin); //bouchon
+        // valeur
+        Date dateDeb = new Date("2011/05/24");
+        Date dateFin = new Date("2011/05/24");
+        boolean valide = true;
+        HashMap<String,Double> result = new HashMap<>();
+        result = myDaoAdmin.CAofCategorie(dateDeb,dateFin);
+        
+        HashMap<String,Double> resultAttendu = new HashMap<>();
+        resultAttendu.put("BK", 6963.375000);
+        resultAttendu.put("CB", 2905.050000);
+        resultAttendu.put("FW", 4272.885000);
+        resultAttendu.put("HW", 282546.390000);
+        resultAttendu.put("SW", 237855.098800);
+        
+        // Vérification du contenu de la table crée
+        for(String key : result.keySet()){
+           // On vérifie si la clé existe
+           if (resultAttendu.containsKey(key)){
+               // ON vérifie le contenu de la clé
+               if(! result.get(key).equals(resultAttendu.get(key))){
+                   valide = false;
+               }
+           } else {
+               valide = false;
+           }
+        }    
+        assertEquals(true, valide);
     }
     
     /**
      * Méthode de test pour obtenir le CA selon une zone géographique
      */
-    @Test
+    @Test @Ignore
     public void testCAZoneGeo() {
         // TODO implementer test + changer val retour
-        String zipCode = "12345";
          Date dateDeb = new Date("2018/11/30");
-        Date dateFin = new Date("2018/11/30");
-         HashMap<String,Double> result = myDaoAdmin.CAofZoneGeo(zipCode, dateDeb, dateFin); //bouchon
+         Date dateFin = new Date("2018/11/30");
+         //HashMap<String,Double> result = myDaoAdmin.CAofZoneGeo(dateDeb, dateFin); //bouchon
     }
     
     /**
      * Méthode de test pour obtenir le CA selon un client
      */
-    @Test
+    @Test @Ignore
     public void testCAClient() {
         //TODO Implementer + changer val retour
-        int codeClient = 5;
         Date dateDeb = new Date("2018/11/30");
         Date dateFin = new Date("2018/11/30");
-         HashMap<String,Double> result = myDaoAdmin.CAfromClient(codeClient, dateDeb, dateFin); //bouchon
+        HashMap<String,Double> result = myDaoAdmin.CAfromClient(dateDeb, dateFin); //bouchon
+
     }
     
-    @Test
+    @Test @Ignore
     public void testInsertProduct() {
+        // Valeur a inserer dans la table produit TODO
+        
         // TODO Implementer + changer val retour
-        int result = myDaoAdmin.insertProduct(); //bouchon
+        myDaoAdmin.insertProduct(); //bouchon
+        
+       // requete qui permet de trouver un produit  : Si la requete select 
+        // fonctionne alors le produit a été inserré sinon => fail de l'insertion
     }
     
+    @Test @Ignore
     public void testUpdateProduct(){
         //TODO implementer + changer val retour
-        int result = myDaoAdmin.updateProduct(); //bouchon
+        myDaoAdmin.updateProduct(); //bouchon
+        
+        // Requete qui permet de verifie si le produti a été update 
+        // => on récupere la nouvelle valeur si elle est égale a la valeur passé 
+        // en param alors ça marche sinon => fail de la mise a jour
     }
     
+    @Test @Ignore
     public void testDeleteProduct(){
         //TODO implementer + changer val retour
-        int result = myDaoAdmin.deleteProduct(); // bouchon
+        myDaoAdmin.deleteProduct(); // bouchon
+        
+        // Reque qui affiche un produit : Si la requete select marche 
+        // alors le produti n'a pas été supprimé => fail de la suppression
+        
     }
     
     
     /* ====================================================================== */
     /* ======================== Test Client ================================= */
-    
 }
 
 
